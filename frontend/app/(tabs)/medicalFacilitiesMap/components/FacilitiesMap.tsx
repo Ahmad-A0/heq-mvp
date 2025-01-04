9471d1a908113f9ddb584458b201ff5ad1c6d06b
Ahmad-A0 <71089234+Ahmad-A0@users.noreply.github.com>
2025-01-04 23:15:40 +0000

    Enhance FacilitiesMap and WeatherWidget components with weather data fetching and improved UI; implement distance and zoom thresholds for weather updates

--- frontend/app/(tabs)/medicalFacilitiesMap/components/FacilitiesMap.tsx
+++ frontend/app/(tabs)/medicalFacilitiesMap/components/FacilitiesMap.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { View, TouchableOpacity, Text } from 'react-native';
 import { SearchFilters } from './SearchFilters';
 import { useFacility } from '../context/FacilityContext';
@@ -27,6 +27,9 @@ interface FacilitiesMapProps {
 
 Mapbox.setAccessToken(MAPBOX_ACCESS_TOKEN);
 
+const DISTANCE_THRESHOLD = 1000; // 1km in meters
+const ZOOM_THRESHOLD = 5;
+
 export function FacilitiesMap({
     userLocation,
     facilities,
@@ -35,6 +38,10 @@ export function FacilitiesMap({
 }: FacilitiesMapProps) {
     const [animateToLocation, setAnimateToLocation] = useState(false);
     const [region, setRegion] = useState<UserLocation | null>(userLocation);
+    const [previousCenter, setPreviousCenter] = useState<number[]>([0, 0]);
+    const [shouldFetchWeather, setShouldFetchWeather] = useState(false);
+    const mapRef = useRef<Mapbox.MapView>(null);
+    const cameraRef = useRef<Mapbox.Camera>(null);
 
     const handleLocationPress = async () => {
         if (userLocation) {
@@ -47,30 +54,74 @@ export function FacilitiesMap({
         }
     };
 
-    const handleRegionChange = (newRegion: any) => {
-        console.log('New region:', newRegion);
-        if (userLocation) {
-            userLocation.latitude = newRegion.geometry.coordinates[1];
-            userLocation.longitude = newRegion.geometry.coordinates[0];
-            setRegion(userLocation);
-        }
-    };
-
     const { searchFilters, onSearchFiltersChange } = useFacility();
+
+    useEffect(() => {
+        const intervalId = setInterval(async () => {
+            console.log('Checking map center and zoom...');
+            if (mapRef.current) {
+                try {
+                    const center = await mapRef.current.getCenter();
+                    const zoom = await mapRef.current.getZoom();
+
+                    const distance = calculateDistance(previousCenter, center);
+
+                    if (
+                        distance > DISTANCE_THRESHOLD &&
+                        zoom > ZOOM_THRESHOLD
+                    ) {
+                        // console.log('Map center or zoom changed:', center, zoom);
+                        setRegion({ latitude: center[1], longitude: center[0] });
+                        setShouldFetchWeather(true);
+                        setPreviousCenter(center);
+                    } else {
+                        // console.log('Map center or zoom did not change:', center, zoom, distance);
+                        setShouldFetchWeather(false);
+                    }
+                } catch (error) {
+                    console.error('Error getting map center or zoom:', error);
+                }
+            }
+        }, 5000);
+
+        return () => clearInterval(intervalId);
+    }, [previousCenter]);
+
+    useEffect(() => {
+        if (shouldFetchWeather && region) {
+            // Fetch updated weather data for region.latitude, region.longitude
+        }
+    }, [shouldFetchWeather, region]);
+
+    function calculateDistance(coord1: number[], coord2: number[]) {
+        const R = 6371e3; // Earth's radius in meters
+        const φ1 = (coord1[1] * Math.PI) / 180; // φ, λ in radians
+        const φ2 = (coord2[1] * Math.PI) / 180;
+        const Δφ = ((coord2[1] - coord1[1]) * Math.PI) / 180;
+        const Δλ = ((coord2[0] - coord1[0]) * Math.PI) / 180;
+
+        const a =
+            Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
+            Math.cos(φ1) * Math.cos(φ2) * Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
+        const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+        return R * c; // in meters
+    }
 
     return (
         <View style={{ flex: 1 }}>
             <MapView
+                ref={mapRef}
                 style={{ flex: 1 }}
                 logoEnabled={false}
                 compassEnabled={false}
                 scaleBarEnabled={false}
                 onPress={onMapPress}
-                onRegionDidChange={handleRegionChange}
             >
                 {userLocation && (
                     <>
                         <Camera
+                            ref={cameraRef}
                             zoomLevel={14}
                             centerCoordinate={[
                                 userLocation.longitude,
@@ -110,7 +161,7 @@ export function FacilitiesMap({
                     />
                 </View>
 
-                <WeatherWidget userLocation={userLocation} className='mx-3 mt-2' />
+                <WeatherWidget userLocation={region} className="mx-3 mt-2" />
 
                 <TouchableOpacity
                     className="mx-3 my-2 p-3 bg-white rounded-full shadow-md"
--- frontend/app/(tabs)/medicalFacilitiesMap/components/WeatherWidget.tsx
+++ frontend/app/(tabs)/medicalFacilitiesMap/components/WeatherWidget.tsx
@@ -2,70 +2,142 @@ import React, { useEffect, useState } from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
 import { useFacility } from '../context/FacilityContext';
 import { UserLocation } from '../types';
-import { Cloud, AlertCircle } from 'lucide-react-native';
+import {
+    Cloud,
+    AlertCircle,
+    Sun,
+    CloudRain,
+    CloudSnow,
+    CloudLightning,
+} from 'lucide-react-native';
 
 interface WeatherData {
-  temp: number;
-  condition: string;
-  alert?: string;
+    temp: number;
+    feelsLike: number;
+    condition: string;
+    humidity: number;
+    windSpeed: number;
+    alert?: string;
 }
 
 interface WeatherWidgetProps {
-  userLocation: UserLocation | null;
-  className?: string;
+    userLocation: UserLocation | null;
+    className?: string;
 }
 
 export function WeatherWidget({ userLocation, className }: WeatherWidgetProps) {
-  const [weather, setWeather] = useState<WeatherData | null>(null);
-  const [alertVisible, setAlertVisible] = useState(false);
+    const [weather, setWeather] = useState<WeatherData | null>(null);
+    const [alertVisible, setAlertVisible] = useState(false);
 
-  useEffect(() => {
-    if (userLocation) {
-      fetchWeather(userLocation.latitude, userLocation.longitude);
-    }
-  }, [userLocation]);
+    useEffect(() => {
+        if (userLocation) {
+            fetchWeather(userLocation.latitude, userLocation.longitude);
+        }
+    }, [userLocation]);
 
-  const fetchWeather = async (lat: number, lon: number) => {
-    try {
-      const response = await fetch(
-        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=34f123866423555abc4c56a78ab3240c&units=metric`
-      );
-      const data = await response.json();
-      
-      const weatherData: WeatherData = {
-        temp: Math.round(data.main.temp),
-        condition: data.weather[0].main,
-      };
+    const fetchWeather = async (lat: number, lon: number) => {
+        try {
+            const response = await fetch(
+                `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=34f123866423555abc4c56a78ab3240c&units=metric`
+            );
+            const data = await response.json();
+            console.info(data);
 
-      if (data.weather[0].id >= 200 && data.weather[0].id < 800) {
-        weatherData.alert = data.weather[0].description;
-        setAlertVisible(true);
-      }
+            const weatherData: WeatherData = {
+                temp: Math.round(data.main.temp),
+                feelsLike: Math.round(data.main.feels_like),
+                condition: data.weather[0].main,
+                humidity: data.main.humidity,
+                windSpeed: data.wind.speed,
+            };
 
-      setWeather(weatherData);
-    } catch (error) {
-      console.error('Error fetching weather:', error);
-    }
-  };
+            if (data.weather[0].id >= 200 && data.weather[0].id < 800) {
+                weatherData.alert = data.weather[0].description;
+                setAlertVisible(true);
+            } else {
+                setAlertVisible(false);
+                weatherData.alert = undefined;
+            }
 
-  return (
-    <TouchableOpacity className={className} onPress={() => alert && alert(weather?.alert)}>
-      {weather && (
-        <View className="bg-white rounded-lg p-3 shadow-md flex-row items-center">
-          {alertVisible && (
-            <TouchableOpacity 
-              className="absolute -top-2 -right-2 bg-red-500 rounded-full p-1"
-              onPress={() => setAlertVisible(false)}
-            >
-              <AlertCircle size={16} color="white" />
-            </TouchableOpacity>
-          )}
-          <Cloud size={24} color="#3B82F6" className="mr-2" />
-          <Text className="text-base">
-            {weather.temp}°C | {weather.condition}
-          </Text>
-        </View>
-      )}
-    </TouchableOpacity>
-  );
+            setWeather(weatherData);
+        } catch (error) {
+            console.error('Error fetching weather:', error);
+        }
+    };
+
+    const getWeatherIcon = (condition: string, className?: string) => {
+        switch (condition) {
+            case 'Clear':
+                return <Sun size={24} color="#FFD700" className={className} />;
+            case 'Clouds':
+                return (
+                    <Cloud size={24} color="#9CB4CC" className={className} />
+                );
+            case 'Rain':
+                return (
+                    <CloudRain
+                        size={24}
+                        color="#3B82F6"
+                        className={className}
+                    />
+                );
+            case 'Snow':
+                return (
+                    <CloudSnow
+                        size={24}
+                        color="#E0F2F1"
+                        className={className}
+                    />
+                );
+            case 'Thunderstorm':
+                return (
+                    <CloudLightning
+                        size={24}
+                        color="#A855F7"
+                        className={className}
+                    />
+                );
+            default:
+                return (
+                    <Cloud size={24} color="#3B82F6" className={className} />
+                );
+        }
+    };
+
+    return (
+        <TouchableOpacity
+            className={className}
+            onPress={() => alertVisible && alert(weather?.alert)}
+        >
+            {weather && (
+                <View className="bg-white rounded-lg p-3 shadow-md flex-row items-center">
+                    {alertVisible && (
+                        <TouchableOpacity
+                            className="absolute -top-2 -right-2 bg-red-500 rounded-full p-1"
+                            onPress={() => setAlertVisible(false)}
+                        >
+                            <AlertCircle size={16} color="white" />
+                        </TouchableOpacity>
+                    )}
+                    {getWeatherIcon(weather.condition)}
+                    <View className="ml-3">
+                        <Text className="text-base">
+                            {weather.temp}°C | {weather.condition}
+                        </Text>
+                        {weather.feelsLike != weather.temp ? (
+                            <Text className="text-xs text-gray-500">
+                                Feels like: {weather.feelsLike}°C
+                            </Text>
+                        ) : null}
+                        <Text className="text-xs text-gray-500">
+                            Wind: {weather.windSpeed} m/s
+                        </Text>
+                        <Text className="text-xs text-gray-500">
+                            Humidity: {weather.humidity}%
+                        </Text>
+                    </View>
+                </View>
+            )}
+        </TouchableOpacity>
+    );
 }
